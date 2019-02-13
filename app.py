621982856a079f010ebc8cd3106c4ab515b7b720
Davide Laezza <truzzialrogo@gmx.com>
2019-02-13 21:50:31 +0100

    Glueing templates with the flask app

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'It works!'
+    return render_template('sidebar/sidebar.jinja',
+                           title='sidebar',
+                           css='test')
 
 
 if __name__ == '__main__':
