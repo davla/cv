47743667cc8f6089c08e63b4996ea02a9bc08638
Davide Laezza <truzzialrogo@gmx.com>
2019-02-14 22:53:12 +0100

    Structuring templates and CSS

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from os import listdir, path
+
+import yaml
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+cv_templates_dir = path.join(app.root_path, app.template_folder, 'templates')
 
-@app.route('/')
-def index():
-    return render_template('sidebar/sidebar.jinja',
-                           title='sidebar',
-                           css='test')
+for cv_template_file in listdir(cv_templates_dir):
+    cv_template_name = path.splitext(cv_template_file)[0]
+    cv_template_jinja_path = path.join('templates', cv_template_file)
+
+
+    @app.route('/templates/{}'.format(cv_template_name))
+    def cv_template_endpoint():
+        with open('info.yaml') as info_file:
+            data = yaml.load(info_file)
+        return render_template(cv_template_jinja_path, css=cv_template_name,
+                               **data)
 
 
 if __name__ == '__main__':
