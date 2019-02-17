99f52b33c77f8d413f367605fe5c7ab184ac36bd
Davide Laezza <truzzialrogo@gmx.com>
2019-02-17 14:39:05 +0100

    Fixing mutiple cv templates issues

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from functools import partial
 from os import listdir, path
 
 import yaml
@@ -8,20 +9,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-cv_templates_dir = path.join(app.root_path, app.template_folder, 'templates')
 
-for cv_template_file in listdir(cv_templates_dir):
-    cv_template_name = path.splitext(cv_template_file)[0]
-    cv_template_jinja_path = path.join('templates', cv_template_file)
+def cv_endpoint(jinja_path, template_name):
+    with open('info.yaml') as info_file:
+        data = yaml.load(info_file)
+    return render_template(jinja_path, css=template_name, **data)
 
 
-    @app.route('/templates/{}'.format(cv_template_name))
-    def cv_template_endpoint():
-        with open('info.yaml') as info_file:
-            data = yaml.load(info_file)
-        return render_template(cv_template_jinja_path, css=cv_template_name,
-                               **data)
+cv_dir = path.join(app.root_path, app.template_folder, 'templates')
 
+for cv_file in listdir(cv_dir):
+    cv_name = path.splitext(cv_file)[0]
+    cv_jinja_path = path.join('templates', cv_file)
+    cv_url = '/templates/{}'.format(cv_name)
+
+    app.add_url_rule(cv_url, cv_name,
+                     partial(cv_endpoint, cv_jinja_path, cv_name))
 
 if __name__ == '__main__':
     app.run(debug=True)
